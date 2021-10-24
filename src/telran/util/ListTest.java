1754dc6000c673a00fc29c0ba7b232c5ad8e5122
Yuragranovsky <yuragranovsky@gmail.com>
2021-10-24 14:49:22 +0300

    adds tests for HW#5

--- src/telran/util/ListTest.java
+++ src/telran/util/ListTest.java
@@ -2,6 +2,8 @@ package telran.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.management.modelmbean.ModelMBeanNotificationBroadcaster;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +27,7 @@ String initialStrings[] = {"name1", "name2"};
 	}
 
 	private List<Integer> getInitialNumbers() {
+		
 		List<Integer> res = new ArrayList<>(1);
 		for (int i = 0; i < initialNumbers.length; i++) {
 			res.add(initialNumbers[i]);
@@ -41,6 +44,49 @@ String initialStrings[] = {"name1", "name2"};
 		
 
 
+	}
+	@Test
+	void testAddAtIndex() {
+		int inserted0 = 100;
+		int inserted2 = -8;
+		int inserted4 = 1000;
+		Integer[] expected = {inserted0, 10, inserted2, 20, 40, inserted4};
+		assertEquals(true, numbers.add(0, inserted0));
+		assertEquals(true, numbers.add(2, inserted2));
+		assertEquals(true, numbers.add(5, inserted4));
+		assertArrayEquals(expected, getArrayFromList(numbers));
+		assertEquals(false, numbers.add(7, 1000));
+		assertEquals(false, numbers.add(-1, 1000));
+	}
+	@Test
+	void testRemove() {
+		Integer expected0[] = {20, 40};
+		Integer expected1[] = {20};
+		assertEquals(null, numbers.remove(3));
+		assertEquals(null, numbers.remove(-1));
+		assertEquals(10, numbers.remove(0));
+		assertArrayEquals(expected0, getArrayFromList(numbers));
+		assertEquals(40, numbers.remove(1));
+		assertArrayEquals(expected1, getArrayFromList(numbers));
+		
+	}
+	@Test 
+	void testSize() {
+		assertEquals(initialNumbers.length, numbers.size());
+		numbers.add(100);
+		assertEquals(initialNumbers.length + 1, numbers.size());
+		numbers.remove(0);
+		assertEquals(initialNumbers.length, numbers.size());
+	}
+	@SuppressWarnings("unchecked")
+	private <T> T[] getArrayFromList(List<T> list) {
+		int size = list.size();
+		T[] res = (T[]) new Object[size];
+		
+		for (int i = 0; i < size; i++) {
+			res[i] = list.get(i);
+		}
+		return res;
 	}
 
 }
